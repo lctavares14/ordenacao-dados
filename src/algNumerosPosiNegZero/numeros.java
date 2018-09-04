b7f505047d84012d1b1891592e2a1041c59738e4
lctavares14 <lctavares14@gmail.com>
2018-09-04 16:16:36 -0400

    lucas

--- src/algNumerosPosiNegZero/numeros.java
+++ src/algNumerosPosiNegZero/numeros.java
@@ -7,12 +7,13 @@ public class numeros {
 		Scanner entrada = new Scanner(System.in);
 		System.out.print("Digite um numero Inteiro: \n");
 		Integer numero = entrada.nextInt();
+		
 		if (numero > 0){ 
 		System.out.println(numero +" é positivo"); 
 		} else if (numero < 0) { 
 		System.out.println(numero+" é negativo"); 
 		} else {
 		System.out.println(numero+" é igual a 0");
+			}	
 		}
-	}
 	}
